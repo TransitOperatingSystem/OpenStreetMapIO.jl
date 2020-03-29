0033fc67b39872f9fa12f5865f5dccff48ef1e7c
jrklasen <jrklasen@gmx.de>
2020-03-29 18:41:22 +0200

    wip

--- src/OpenStreetMapIO.jl
+++ src/OpenStreetMapIO.jl
@@ -10,10 +10,14 @@ MAX_UNCOMPRESSED_BLOB_SIZE = 33554432 # 32 * 1024 * 1024
 include("proto/fileformat_pb.jl") # https://github.com/openstreetmap/OSM-binary/blob/master/src/fileformat.proto
 include("proto/osmformat_pb.jl")  # https://github.com/openstreetmap/OSM-binary/blob/master/src/osmformat.proto
 
-function readpbf(file::String)
+
+function readpbf(file::String, nodecallback=nodecallback, waycallback=waycallback,relationcallback=relationcallback)
     io = open(file)
+    c = 0
     
-    data = []
+    nodeoutput = []
+    wayoutput = []
+    relationoutput = []
     while !eof(io)
         # the first 4 bytes are the header size (in big-endianness)
         headersize = Int64(reinterpret(UInt32, read!(io, Array{UInt8}(undef, 4))[4:-1:1])[1])
@@ -25,7 +29,7 @@ function readpbf(file::String)
         blobheader = readproto(PipeBuffer(read!(io, Array{UInt8}(undef, headersize))), BlobHeader())
         blobsize = blobheader.datasize
         if blobsize > MAX_UNCOMPRESSED_BLOB_SIZE
-            DomainError("blob-size is bigger than allowed $(blobsize) >  $(MAX_UNCOMPRESSED_BLOB_SIZE)")
+            DomainError("blob-size is bigger than allowed $(blobsize) > $(MAX_UNCOMPRESSED_BLOB_SIZE)")
         end
         
         # Read blob
@@ -43,11 +47,166 @@ function readpbf(file::String)
         if blob.raw_size > length(blobdata)
             DomainError("blob reports wrong raw_size: $(blob.raw_size) bytes")
         end
-        
-        append!(data, [(blobheader, blobdata)])
+
+
+        if blobheader._type == "OSMData"
+            primitiveblock = readproto(PipeBuffer(blobdata), PrimitiveBlock())
+            # if c == 371
+            #     return primitiveblock
+            # end
+            # c += 1
+            # Parse blob data
+            # for each primitive group
+            for primitivegroup in primitiveblock.primitivegroup
+                if isdefined(primitivegroup, :nodes) && !isempty(primitivegroup.nodes)
+                    parsenodes!(nodeoutput, primitiveblock, primitivegroup.nodes, nodecallback)
+                elseif isdefined(primitivegroup, :dense) && !isempty(primitivegroup.dense.id)
+                    parsedensenodes!(nodeoutput, primitiveblock, primitivegroup.dense, nodecallback)
+                elseif isdefined(primitivegroup, :ways) && !isempty(primitivegroup.ways)
+                    parseways!(wayoutput, primitiveblock, primitivegroup.ways, waycallback)
+                elseif isdefined(primitivegroup, :rlations) && !isempty(primitivegroup.relations)
+                    parserelations!(relationoutput, primitiveblock, primitivegroup.relations, relationcallback)
+                end
+            end
+        # elseif blobheader._type == "OSMHeader"
+        #
+        # else
+        #     DomainError("Unknown blob type: " * blobheader._type)
+        end
     end
     
-    return data
+    return Dict("nodes" => nodeoutput, "ways" =>wayoutput, "relations" => relationoutput)
+end
+
+
+function parsenodes!(output, primitiveblock, nodes, nodecallback=nodecallback)#, changesetcallback)
+    for node in nodes
+        lat = 1e-9 * (primitiveblock.lat_offset + primitiveblock.granularity * node.lat)
+        lon = 1e-9 * (primitiveblock.lon_offset + primitiveblock.granularity * node.lon)
+
+        # get tags 
+        tags = Dict()
+        for kv_inx in 1:length(nodes.keys_vals)
+            key = String(copy(primitiveblock.stringtable.s[nodes.keys[kv_inx]]))
+            value = String(copy(primitiveblock.stringtable.s[nodes.vals[kv_inx]]))
+            tags[key] = value
+        end
+        
+        append!(output, nodecallback(node.id, lat, lon, tags))
+
+        # if hasvalues(nods.info) && hasvalues(node.info.changeset) #&& (interest & CHANGESETS) == CHANGESETS)
+        #     #changesetcallback(node.info.changeset)
+        # end
+    end
+end
+
+function parsedensenodes!(output, primitiveblock, densenodes, nodecallback=nodecallback)#, changesetcallback)
+    id::Int64 = 0
+    latitude::Float64 = 0.0
+    longitude::Float64 = 0.0
+    
+    kv_inx::Int64 = 1
+    for n_inx in 1:length(densenodes.id)
+        id += densenodes.id[n_inx]
+        latitude += 1e-9 * (primitiveblock.lat_offset + primitiveblock.granularity * densenodes.lat[n_inx])
+        longitude += 1e-9 * (primitiveblock.lon_offset + primitiveblock.granularity * densenodes.lon[n_inx])
+
+        # get tags
+        tags = Dict()
+        while kv_inx <= length(densenodes.keys_vals) && densenodes.keys_vals[kv_inx] != 0
+            key = String(copy(primitiveblock.stringtable.s[densenodes.keys_vals[kv_inx]]))
+            value = String(copy(primitiveblock.stringtable.s[densenodes.keys_vals[kv_inx + 1]]))
+            kv_inx += 2
+            tags[key] = value
+        end
+        kv_inx += 1
+
+        append!(output, nodecallback(id, latitude, longitude, tags))
+    end
+
+    # if hasvalues(densenodes.denseinfo) # && (interest & CHANGESETS) == CHANGESETS)
+    #     changeset = 0
+    #     for cs in densenodes.denseinfo.changeset
+    #         #changesetcallback(changeset += cs)
+    #     end
+    # end
+end
+
+
+function parseways!(output, primitiveblock, ways, waycallback=waycallback)#, changesetcallback)
+    for way in ways 
+        node = 0
+        nodes = []
+        
+        for node_id_offset in way.refs
+            node += node_id_offset
+            append!(nodes, node)
+        end
+        
+        # get tags 
+        tags = Dict()
+        for kv_inx in 1:length(way.keys)
+            key = String(copy(primitiveblock.stringtable.s[way.keys[kv_inx]]))
+            value = String(copy(primitiveblock.stringtable.s[way.vals[kv_inx]]))
+            tags[key] = value;
+        end
+        
+        append!(output, waycallback(way.id, tags, nodes))
+        
+        # if hasvalues(way.info) && hasvalues(way.info.changeset) #&& (interest & CHANGESETS) == CHANGESETS)
+        #     #changesetcallback(way.info.changeset)
+        # end
+    end
+end
+
+
+function parserelations!(output, primitiveblock, relations, relationcallback=relationcallback)#, changesetcallback)
+    for relation in relations
+        member = 0
+        members = []
+        for m_inx in relation.memids_size
+            member += relation.memids[m_inx];
+            append!(members, [relation.types[m_inx], member, primitiveblock.stringtable.s[relation.roles_sid[m_inx]]])
+        end
+        
+        # get tags 
+        tags = Dict()
+        for kv_inx in 1:length(relation.keys_vals)
+            key = String(copy(primitiveblock.stringtable.s[relation.keys[kv_inx]]))
+            value = String(copy(primitiveblock.stringtable.s[relation.vals[kv_inx]]))
+            tags[key] = value;
+        end
+        
+        append!(output, relationcallback(relation.id, tags, members))
+        
+        # if hasvalues(relation.info) && hasvalues(relation.info.changeset) #&& (interest & CHANGESETS) == CHANGESETS)z
+        #     #changesetcallback(relation.info.changeset)
+        # end
+    end
+end
+
+
+function nodecallback(id, latitude, longitude, tags)
+    return [id, latitude, longitude, tags]
+end
+
+
+function waycallback(id, tags, nodes)
+    return [id, tags, nodes]
+end
+
+
+function relationcallback(id, tags, members)
+    return [id, tags, members]
+end
+
+function hasvalues(field)
+    empty = true
+    try
+        empty = !isempty(field)
+    finally
+        return empty
+    end
 end
 
 end # module
